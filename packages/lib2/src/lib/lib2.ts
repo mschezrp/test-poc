a920a8e8856aafdf26471b8e688947494afca921
Miguel Sánchez <miguel.sanchez@shijigroup.com>
2023-07-12 15:22:34 +0200

    Implement lib4

--- packages/lib2/src/lib/lib2.ts
+++ packages/lib2/src/lib/lib2.ts
@@ -1,6 +1,7 @@
 import { lib1 } from 'test-lib1';
 import { lib3 } from 'test-lib3';
+import { lib4 } from 'test-lib4';
 
 export function lib2(): string {
-  return 'lib2-' + lib1() + '-' + lib3();
+  return 'lib2-' + lib1() + '-' + lib3() + '-' + lib4();
 }
